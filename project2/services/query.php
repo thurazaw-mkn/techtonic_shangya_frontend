b71181ce5cfefc9d5f598f844506aa6da5277da6
Sai Lyan Hein <j23040369@student.newinti.edu.my>
2025-06-18 00:19:03 +0800

    minor changes

--- project2/services/query.php
+++ project2/services/query.php
@@ -1,7 +1,8 @@
 <?php
 require_once(__DIR__ . '/../settings.php');
 
-function get_admin_by_credentials($username, $password) {
+function get_admin_by_credentials($username, $password)
+{
     global $host, $user, $pwd, $sql_db;
     $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
 
@@ -15,6 +16,96 @@ function get_admin_by_credentials($username, $password) {
     // Use prepared statements to prevent SQL injection
     $stmt = $conn->prepare("SELECT username, role, display_name FROM admins WHERE username = ? AND password = ?");
     $stmt->bind_param("ss", $username, $hashed_password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $admin = $result->fetch_assoc();
+
+    $stmt->close();
+    $conn->close();
+
+    return $admin;
+}
+
+function get_job_with_filter($position_keyword, $location_keyword, $page_size, $page_number)
+{
+    global $host, $user, $pwd, $sql_db;
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        return null;
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("SELECT * from jobs WHERE position = $position_keyword or location = $location_keyword");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $admin = $result->fetch_assoc();
+
+    $stmt->close();
+    $conn->close();
+
+    return $admin;
+}
+
+function create_job($job_ref_number, $title, $position, $location, $requirement_essential, $requirement_preferable, $salary_range, $description, $created_at, $created_by, $updated_at, $updated_by)
+{
+    global $host, $user, $pwd, $sql_db;
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        return null;
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("INSERT into jobs (job_ref_lnumber, title, position, location, requirement_essential, requirement_preferable, salary_range, description, created_at, created_by, updated_at, updated_by) values (?,?,?,?,?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("issssssssss", $job_ref_number, $title, $position, $location, $requirement_essential, $requirement_preferable, $salary_range, $description, $created_at, $created_by, $updated_at, $updated_by);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $admin = $result->fetch_assoc();
+
+    $stmt->close();
+    $conn->close();
+
+    return $admin;
+}
+
+function get_job($position_keyword, $location_keyword, $page_size, $page_number)
+{
+    global $host, $user, $pwd, $sql_db;
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        return null;
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("SELECT * from jobs");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $admin = $result->fetch_assoc();
+
+    $stmt->close();
+    $conn->close();
+
+    return $admin;
+}
+
+function update_job($id, $job_ref_number, $title, $position, $location, $requirement_essential, $requirement_preferable, $salary_range, $description, $created_at, $created_by, $updated_at, $updated_by)
+{
+    global $host, $user, $pwd, $sql_db;
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        return null;
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("UPDATE jobs SET job_ref_lnumber = ?, title = ?, position = ?, location = ?, requirement_essential = ?, requirement_preferable = ?, salary_range = ?, description = ?, created_at = ?, created_by = ?, updated_at = ?, updated_by = ? WHERE id = ?");
+    $stmt->bind_param("isssssssssssi", $job_ref_number, $title, $position, $location, $requirement_essential, $requirement_preferable, $salary_range, $description, $created_at, $created_by, $updated_at, $updated_by, $id);
     $stmt->execute();
     $result = $stmt->get_result();
 
