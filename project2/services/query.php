d1de637740d07cf29b449a74dbffe6642b94ba97
Thura Zaw <>
2025-06-18 00:12:30 +0800

    query for eoi

--- project2/services/query.php
+++ project2/services/query.php
@@ -25,4 +25,80 @@ function get_admin_by_credentials($username, $password) {
 
     return $admin;
 }
+
+function create_eoi($job_reference_number, $first_name, $last_name, $address_street, $address_city, $address_state, $address_postcode, $email, $phone, $skills, $other_skills, $status, $created_at, $created_by, $updated_at, $updated_by) 
+{
+    global $host, $user, $pwd, $sql_db;
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        return null;
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("INSERT INTO eoi (job_reference_number, first_name, last_name, address_street, address_city, address_state, address_postcode, email, phone, skills, other_skills, status, created_at, created_by, updated_at, updated_by) 
+    values (?,?,?,?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssssssssss", $job_reference_number, $first_name, $last_name, $address_street, $address_city, $address_state, $address_postcode, $email, $phone, $skills, $other_skills, $status, $created_at, $created_by, $updated_at, $updated_by);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $admin = $result->fetch_assoc();
+
+    $stmt->close();
+    $conn->close();
+
+    return $admin;
+}
+
+function get_eoi() 
+{
+    global $host, $user, $pwd, $sql_db;
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        return null;
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare(" SELECT * FROM eoi");
+
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $admin = $result->fetch_assoc();
+
+    $stmt->close();
+    $conn->close();
+
+    return $admin;
+}
+
+function get_eoi_with_filter($job_reference_number, $first_name, $last_name) 
+{
+    global $host, $user, $pwd, $sql_db;
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+    if (!$conn) {
+        return null;
+    }
+
+    // Use prepared statements to prevent SQL injection
+    $stmt = $conn->prepare("SELECT * FROM eoi WHERE job_reference_number = ? OR first_name = ? OR last_name = ?");
+    $stmt->bind_param("sss", $job_reference_number, $first_name, $last_name);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    $admin = $result->fetch_assoc();
+
+    $stmt->close();
+    $conn->close();
+
+    return $admin;
+}
+
+
+
+
+
+
 ?>
